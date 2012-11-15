bd023512ac99136e57789d1be3742dd5c5994e0c
Michael Brooks <mjbrooks@uw.edu>
2012-11-15 14:22:15 -0800

    Partly filled in exampleset and tests

--- src/etc/aloe/data/ExampleSet.java
+++ src/etc/aloe/data/ExampleSet.java
@@ -1,23 +1,56 @@
 package etc.aloe.data;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.instance.RemoveWithValues;
+
 /**
  * ExampleSet contains information about data points that have features
  * extracted. These data points are ready for labeling by a model.
  */
-public abstract class ExampleSet {
+public class ExampleSet {
+
+    Instances instances;
+
+    public ExampleSet(Instances instances) {
+        this.instances = instances;
+    }
 
     /**
-     * The size of the entity set.
+     * The size of the example set.
+     *
      * @return
      */
-    public abstract int size();
+    public int size() {
+        return instances.size();
+    }
 
     /**
      * Returns a new example set containing only those examples with labels.
+     *
      * @return
      */
     public ExampleSet onlyLabeled() {
-        throw new UnsupportedOperationException("Not yet implemented");
+        RemoveWithValues filter = new RemoveWithValues();
+        filter.setAttributeIndex("" + (instances.classIndex() + 1));
+        filter.setMatchMissingValues(true);
+        filter.setInvertSelection(true);
+
+        try {
+            filter.setInputFormat(instances);
+            Instances result = Filter.useFilter(instances, filter);
+            ExampleSet resultSet = new ExampleSet(result);
+            return resultSet;
+        } catch (Exception ex) {
+            System.err.println("Unable to apply filter!");
+            return null;
+        }
     }
 
+    Instance get(int i) {
+        return instances.get(i);
+    }
 }
---
+++ test/etc/aloe/data/ExampleSetTest.java
@@ -0,0 +1,132 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package etc.aloe.data;
+
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.Utils;
+
+/**
+ *
+ * @author michael
+ */
+public class ExampleSetTest {
+
+    private Instances instances;
+    private int numMissingClass;
+
+    public ExampleSetTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        ArrayList<Attribute> attributes = new ArrayList<Attribute>();
+
+        attributes.add(new Attribute("id"));
+
+        ArrayList<String> classValues = new ArrayList<String>();
+        classValues.add("false");
+        classValues.add("true");
+        attributes.add(new Attribute("class", classValues));
+
+        this.instances = new Instances("TestInstances", attributes, 12);
+        this.instances.setClassIndex(1);
+
+        this.instances.add(new DenseInstance(1.0, new double[]{1.0, 1.0}));
+        this.instances.add(new DenseInstance(1.0, new double[]{2.0, Utils.missingValue()}));
+        this.instances.add(new DenseInstance(1.0, new double[]{3.0, 1.0}));
+        this.instances.add(new DenseInstance(1.0, new double[]{4.0, 1.0}));
+        this.instances.add(new DenseInstance(1.0, new double[]{5.0, 1.0}));
+        this.instances.add(new DenseInstance(1.0, new double[]{6.0, 1.0}));
+        this.instances.add(new DenseInstance(1.0, new double[]{7.0, 1.0}));
+        this.instances.add(new DenseInstance(1.0, new double[]{8.0, 1.0}));
+        this.instances.add(new DenseInstance(1.0, new double[]{9.0, Utils.missingValue()}));
+        this.instances.add(new DenseInstance(1.0, new double[]{10.0, 1.0}));
+        this.instances.add(new DenseInstance(1.0, new double[]{11.0, 1.0}));
+        this.instances.add(new DenseInstance(1.0, new double[]{12.0, Utils.missingValue()}));
+
+        numMissingClass = 3;
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of size method, of class ExampleSet.
+     */
+    @Test
+    public void testSize() {
+        System.out.println("size");
+        ExampleSet instance = new ExampleSet(this.instances);
+        int expResult = this.instances.size();
+        int result = instance.size();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of onlyLabeled method, of class ExampleSet.
+     */
+    @Test
+    public void testOnlyLabeled() {
+        System.out.println("onlyLabeled");
+
+        ExampleSet examples = new ExampleSet(this.instances);
+        ExampleSet labeled = examples.onlyLabeled();
+
+        assertEquals("labeled set is the right size", instances.size() - numMissingClass, labeled.size());
+
+        int classAttr = instances.classIndex();
+        for (int i = 0; i < labeled.size(); i++) {
+            Instance instance = labeled.get(i);
+            assertEquals(false, instance.isMissing(classAttr));
+        }
+    }
+
+    /**
+     * Test of get method, of class ExampleSet.
+     */
+    @Test
+    public void testGet() {
+        System.out.println("get");
+
+        ExampleSet instance = new ExampleSet(this.instances);
+        Instance expResult = null;
+
+        assertNotNull(instance.get(0));
+        assertNotNull(instance.get(this.instances.size() - 1));
+
+        try {
+            instance.get(-1);
+            assertFalse("Not successful", true);
+        } catch (IndexOutOfBoundsException e) {
+            assertTrue("Throws out of bounds exception on negative indices", true);
+        }
+
+        try {
+            instance.get(this.instances.size());
+            assertFalse("Not successful", true);
+        } catch (IndexOutOfBoundsException e) {
+            assertTrue("Throws out of bounds exception on out of range indices", true);
+        }
+    }
+}
