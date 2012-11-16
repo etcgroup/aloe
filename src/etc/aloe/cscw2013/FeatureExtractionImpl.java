dce6c0030853bc1ecc780a92d2ae0895e9a31189
Michael Brooks <mjbrooks@uw.edu>
2012-11-15 22:08:27 -0800

    feature extraction impl dummy

--- src/etc/aloe/cscw2013/FeatureExtractionImpl.java
+++ src/etc/aloe/cscw2013/FeatureExtractionImpl.java
@@ -4,9 +4,6 @@ import etc.aloe.data.ExampleSet;
 import etc.aloe.data.FeatureSpecification;
 import etc.aloe.data.SegmentSet;
 import etc.aloe.processes.FeatureExtraction;
-import java.util.ArrayList;
-import weka.core.Attribute;
-import weka.core.Instances;
 
 /**
  *
@@ -15,8 +12,7 @@ public class FeatureExtractionImpl implements FeatureExtraction {
 
     @Override
     public ExampleSet extractFeatures(SegmentSet segments, FeatureSpecification spec) {
-        Instances instances = new Instances("Example Set", new ArrayList<Attribute>(), 0);
-        ExampleSet examples = new ExampleSet(instances);
-        return examples;
+        //TODO: Fill me in!
+        return segments.getBasicExamples();
     }
 }
