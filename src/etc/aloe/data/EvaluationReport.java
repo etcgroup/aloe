fca5c46997c9529817f149af931e85ec01e2a44a
Michael Brooks <mjbrooks@uw.edu>
2012-11-15 17:19:02 -0800

    fill in skeleton evaluation report

--- src/etc/aloe/data/EvaluationReport.java
+++ src/etc/aloe/data/EvaluationReport.java
@@ -3,20 +3,35 @@ package etc.aloe.data;
 import etc.aloe.processes.Saving;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 /**
- * The EvaluationReport contains data about model performance as compared to
- * a source of truth data.
+ * The EvaluationReport contains data about model performance as compared to a
+ * source of truth data.
  */
 public class EvaluationReport implements Saving {
 
+    int truePositiveCount;
+    int trueNegativeCount;
+    int falsePositiveCount;
+    int falseNegativeCount;
+
     @Override
-    public boolean save(File destination) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public boolean save(OutputStream destination) throws IOException {
+        PrintStream out = new PrintStream(destination);
+        //TODO: implement me better!
+        out.println("TP: " + truePositiveCount);
+        out.println("FP: " + falsePositiveCount);
+        out.println("TN: " + trueNegativeCount);
+        out.println("FN: " + falseNegativeCount);
+        return true;
     }
 
     public void addPartial(EvaluationReport report) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        truePositiveCount += report.truePositiveCount;
+        trueNegativeCount += report.trueNegativeCount;
+        falsePositiveCount += report.falsePositiveCount;
+        falseNegativeCount += report.falseNegativeCount;
     }
-
 }
