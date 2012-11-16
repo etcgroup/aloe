51aaa488038112298b80c4eecb0ec754d2ace182
Michael Brooks <mjbrooks@uw.edu>
2012-11-15 17:20:18 -0800

    input/outputstream for feature spec

--- src/etc/aloe/cscw2013/FeatureSpecificationImpl.java
+++ src/etc/aloe/cscw2013/FeatureSpecificationImpl.java
@@ -1,11 +1,11 @@
 package etc.aloe.cscw2013;
 
 import etc.aloe.data.FeatureSpecification;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InvalidObjectException;
+import java.io.OutputStream;
 import java.io.PrintStream;
 
 /**
@@ -15,13 +15,13 @@ import java.io.PrintStream;
 public class FeatureSpecificationImpl extends FeatureSpecification {
 
     @Override
-    public boolean load(File source) throws FileNotFoundException, InvalidObjectException {
+    public boolean load(InputStream source) throws FileNotFoundException, InvalidObjectException {
         //TODO: fill me in!
         return true;
     }
 
     @Override
-    public boolean save(File destination) throws IOException {
+    public boolean save(OutputStream destination) throws IOException {
         PrintStream writer = new PrintStream(destination);
         writer.println("nothing to do here");
         //TODO: fill me in!
