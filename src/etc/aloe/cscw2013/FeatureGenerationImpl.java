6239a068356ad7fb7fe35259b6e48739487e62ab
Michael Brooks <mjbrooks@uw.edu>
2012-11-24 16:00:46 -0800

    feature generation implementation and tests

--- src/etc/aloe/cscw2013/FeatureGenerationImpl.java
+++ src/etc/aloe/cscw2013/FeatureGenerationImpl.java
@@ -3,7 +3,19 @@ package etc.aloe.cscw2013;
 import etc.aloe.data.ExampleSet;
 import etc.aloe.data.FeatureSpecification;
 import etc.aloe.filters.PronounRegexFilter;
+import etc.aloe.filters.PunctuationRegexFilter;
+import etc.aloe.filters.SimpleStringToWordVector;
+import etc.aloe.filters.SimpleStringToWordVector.NoNonsenseStemmer;
+import etc.aloe.filters.SpecialRegexFilter;
+import etc.aloe.filters.SpellingRegexFilter;
+import etc.aloe.filters.StringToDictionaryVector;
 import etc.aloe.processes.FeatureGeneration;
+import java.io.File;
+import java.util.List;
+import weka.core.Instances;
+import weka.core.SelectedTag;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.StringToWordVector;
 
 /**
  *
@@ -11,15 +23,121 @@ import etc.aloe.processes.FeatureGeneration;
  */
 public class FeatureGenerationImpl implements FeatureGeneration {
 
+    private static final boolean COUNT_REGEX_LENGTHS = true;
+    private static final String EMOTICON_FEATURE_PREFIX = "#";
+    private static final String BAG_OF_WORDS_FEATURE_PREFIX = "_";
+    private final List<String> emoticonDictionary;
+
+    public FeatureGenerationImpl(List<String> emoticonDictionary) {
+        this.emoticonDictionary = emoticonDictionary;
+    }
+
     @Override
     public FeatureSpecification generateFeatures(ExampleSet basicExamples) {
 
+        ExampleSet examples = basicExamples.copy();
         FeatureSpecification spec = new FeatureSpecification();
 
-        //TODO: fill me in!
-        spec.addFilter(new PronounRegexFilter(ExampleSet.MESSAGE_ATTR_NAME));
+        try {
+            spec.addFilter(getPronounsFilter(examples));
+            spec.addFilter(getPunctuationFilter(examples));
+            spec.addFilter(getSpecialWordsFilter(examples));
+            spec.addFilter(getSpellingFilter(examples));
+
+            spec.addFilter(getEmoticonsFilter(examples));
+            spec.addFilter(getBagOfWordsFilter(examples));
+        } catch (Exception e) {
+            System.err.println("Error generating features.");
+            System.err.println("\t" + e.getMessage());
+        }
 
         return spec;
     }
 
+    private Filter getSpecialWordsFilter(ExampleSet examples) throws Exception {
+        SpecialRegexFilter filter = new SpecialRegexFilter(ExampleSet.MESSAGE_ATTR_NAME);
+
+        filter.setInputFormat(examples.getInstances());
+        Instances filtered = Filter.useFilter(examples.getInstances(), filter);
+        examples.setInstances(filtered);
+
+        return filter;
+    }
+
+    private Filter getSpellingFilter(ExampleSet examples) throws Exception {
+        SpellingRegexFilter filter = new SpellingRegexFilter(ExampleSet.MESSAGE_ATTR_NAME);
+        filter.setCountRegexLengths(COUNT_REGEX_LENGTHS);
+
+        filter.setInputFormat(examples.getInstances());
+        Instances filtered = Filter.useFilter(examples.getInstances(), filter);
+        examples.setInstances(filtered);
+
+        return filter;
+    }
+
+    private Filter getPunctuationFilter(ExampleSet examples) throws Exception {
+        PunctuationRegexFilter filter = new PunctuationRegexFilter(ExampleSet.MESSAGE_ATTR_NAME);
+        filter.setCountRegexLengths(COUNT_REGEX_LENGTHS);
+
+        filter.setInputFormat(examples.getInstances());
+        Instances filtered = Filter.useFilter(examples.getInstances(), filter);
+        examples.setInstances(filtered);
+
+        return filter;
+    }
+
+    private Filter getPronounsFilter(ExampleSet examples) throws Exception {
+        PronounRegexFilter filter = new PronounRegexFilter(ExampleSet.MESSAGE_ATTR_NAME);
+
+        filter.setInputFormat(examples.getInstances());
+        Instances filtered = Filter.useFilter(examples.getInstances(), filter);
+        examples.setInstances(filtered);
+
+        return filter;
+    }
+
+    private Filter getEmoticonsFilter(ExampleSet examples) throws Exception {
+        StringToDictionaryVector filter = new StringToDictionaryVector();
+        filter.setAttributeNamePrefix(EMOTICON_FEATURE_PREFIX);
+        filter.setTermList(emoticonDictionary);
+        filter.setStringAttribute(ExampleSet.MESSAGE_ATTR_NAME);
+        filter.setWordsToKeep(10000);//don't want this to limit things
+
+        filter.setMinTermFreq(10);
+        filter.setDoNotOperateOnPerClassBasis(true);
+        filter.setOutputWordCounts(true);
+
+        filter.setInputFormat(examples.getInstances());
+        Instances filtered = Filter.useFilter(examples.getInstances(), filter);
+        examples.setInstances(filtered);
+
+        return filter;
+    }
+
+    private Filter getBagOfWordsFilter(ExampleSet examples) throws Exception {
+        SimpleStringToWordVector filter = new SimpleStringToWordVector();
+        filter.setAttributeNamePrefix(BAG_OF_WORDS_FEATURE_PREFIX);
+        filter.setStringAttributeName(ExampleSet.MESSAGE_ATTR_NAME);
+
+        //This is stupid because it depends on how much data you use
+        //bagger.setMinTermFreq(20);
+
+        filter.setWordsToKeep(600);
+        filter.setLowerCaseTokens(true);
+
+        //use stemming and remove "nonsense"
+        filter.setStemmer(new NoNonsenseStemmer(true));
+
+        filter.setTFTransform(true);
+        filter.setIDFTransform(true);
+        filter.setNormalizeDocLength(new SelectedTag(StringToWordVector.FILTER_NORMALIZE_ALL, StringToWordVector.TAGS_FILTER));
+
+        filter.setOutputWordCounts(true);
+
+        filter.setInputFormat(examples.getInstances());
+        Instances filtered = Filter.useFilter(examples.getInstances(), filter);
+        examples.setInstances(filtered);
+
+        return filter;
+    }
 }
--- test/etc/aloe/cscw2013/FeatureGenerationImplTest.java
+++ test/etc/aloe/cscw2013/FeatureGenerationImplTest.java
@@ -4,20 +4,31 @@
  */
 package etc.aloe.cscw2013;
 
+import etc.aloe.data.ExampleSet;
 import etc.aloe.data.FeatureSpecification;
+import etc.aloe.data.Message;
+import etc.aloe.data.Segment;
 import etc.aloe.data.SegmentSet;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import weka.filters.Filter;
 
 /**
  *
  * @author michael
  */
 public class FeatureGenerationImplTest {
+
+    private List<String> termList;
+    private SegmentSet segments;
+    private ExampleSet basicExamples;
 
     public FeatureGenerationImplTest() {
     }
@@ -32,6 +43,36 @@ public class FeatureGenerationImplTest {
 
     @Before
     public void setUp() {
+        termList = Arrays.asList(new String[]{
+                    ":)",
+                    ":-)",
+                    ":-(",
+                    ":>",
+                    ";)",
+                    ";-)"
+                });
+
+        segments = new SegmentSet();
+
+        Segment seg0 = new Segment();
+        seg0.add(new Message(0, new Date(), "Alice", "it's"));
+        seg0.add(new Message(1, new Date(), "Bob", "cow"));
+        seg0.add(new Message(2, new Date(), "Alice", "time"));
+        seg0.setTrueLabel(Boolean.TRUE);
+        segments.add(seg0);
+
+        Segment seg1 = new Segment();
+        seg1.add(new Message(3, new Date(), "Bob", "noooooooo"));
+        seg1.setTrueLabel(Boolean.FALSE);
+        segments.add(seg1);
+
+        Segment seg2 = new Segment();
+        seg2.add(new Message(4, new Date(), "Bob", "once"));
+        seg2.add(new Message(5, new Date(), "Alice", "upon"));
+        seg2.setTrueLabel(Boolean.FALSE);
+        segments.add(seg2);
+
+        basicExamples = segments.getBasicExamples();
     }
 
     @After
@@ -44,12 +85,22 @@ public class FeatureGenerationImplTest {
     @Test
     public void testGenerateFeatures() {
         System.out.println("generateFeatures");
-        SegmentSet segments = null;
-        FeatureGenerationImpl instance = new FeatureGenerationImpl();
-        FeatureSpecification expResult = null;
-//        FeatureSpecification result = instance.generateFeatures(segments);
-//        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+        FeatureGenerationImpl generation = new FeatureGenerationImpl(termList);
+        FeatureSpecification spec = generation.generateFeatures(basicExamples);
+
+        assertNotNull(spec);
+        assertTrue(spec.getFilters().size() > 0);
+
+        for (Filter filter : spec.getFilters()) {
+            assertTrue(filter.isOutputFormatDefined());
+            assertTrue(filter.isFirstBatchDone());
+
+            try {
+                basicExamples.setInstances(Filter.useFilter(basicExamples.getInstances(), filter));
+            } catch (Exception e) {
+                assertTrue(e.getMessage(), false);
+            }
+        }
     }
 }
