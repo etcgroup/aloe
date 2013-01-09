e149c9a295cbd51b8eeb78a259c6fc97fc78b2a7
Michael Brooks <mjbrooks@uw.edu>
2013-01-08 17:16:03 -0800

    add class for representing labels

---
+++ src/etc/aloe/data/Label.java
@@ -0,0 +1,86 @@
+/*
+ * This file is part of ALOE.
+ *
+ * ALOE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * ALOE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with ALOE.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2012 SCCL, University of Washington (http://depts.washington.edu/sccl)
+ */
+package etc.aloe.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a label for a message/segment.
+ *
+ * @author Michael Brooks <mjbrooks@uw.edu>
+ */
+public class Label {
+
+    private final String name;
+    private final int number;
+    private static List<Label> values = new ArrayList<Label>();
+
+    private Label(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public static Label get(String name) {
+        for (Label label : values) {
+            if (label.getName().equals(name)) {
+                return label;
+            }
+        }
+
+        Label label = new Label(name, values.size());
+        values.add(label);
+        return label;
+    }
+
+    public static Label get(int number) {
+        if (number >= 0 && number < values.size()) {
+            return values.get(number);
+        }
+
+        Label label = new Label(number + "", values.size());
+        values.add(label);
+        return label;
+    }
+
+    public static boolean exists(String name) {
+        for (Label label : values) {
+            if (label.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean exists(int number) {
+        if (number >= 0 && number < values.size()) {
+            return true;
+        }
+        
+        return false;
+    }
+}
