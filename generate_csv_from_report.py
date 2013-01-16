808578e5018412f853831ed94f159c575e653e86
Daniel Barella <dan.barella@gmail.com>
2013-01-16 10:57:26 -0800

    Initial commit. Code includes basic documentation, and a pythonized version of the ALOE disclaimer.

---
+++ generate_csv_from_report.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""
+This is a script that takes an ALOE report file and generates a CSV row. 
+
+This script can be used in conjunction with the cat utility to compound an archive of results
+for data analysis in spreadsheet software. Note that this is written in Python 3.
+"""
+
+__status__ = "Prototype"
+__author__ = "Daniel Barella"
+__email__ = "dan.barella@gmail.com"
+__copyright__ = "Copyright (c) 2013 SCCL, University of Washington (http://depts.washington.edu/sccl)"
+__license__ = "GPL"
+__version__ = "0.1"
