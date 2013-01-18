d5aa0d8fd296511a5b16752bb20e08bbfcb96960
Daniel Barella <dan.barella@gmail.com>
2013-01-17 16:33:45 -0800

    Added controller script with basic comments and pseudocode runthrough  I'm still thinking the mechanics through, so this is bound to change.

---
+++ run_pipelines.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+""" 
+This is a batch script that runs the ALOE classifier with the 
+CSCW2013 and HeatSegmentation pipelines on a set of data files.
+It then runs the report files through a python script to generate
+a csv data file, prepended with the affect code and pipeline. 
+
+Incidentally,
+--- 
+This file is part of ALOE.
+
+ALOE is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ALOE is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ALOE.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__status__ = "Prototype"
+__author__ = "Daniel Barella"
+__email__ = "dan.barella@gmail.com"
+__copyright__ = "Copyright (c) 2013 SCCL, University of Washington (http://depts.washington.edu/sccl)"
+__license__ = "GPL"
+__version__ = "0.1"
+
+import sys
+import subprocess
+
+"""
+Pseudocode runthrough:
+
+We'll need to have the aloe, input and output directories
+From there, we need a list of the pipelines to run, and potentially the arguments to give as well
+Make sure that there is no outfile already - throw an error if there is
+ Create a blank outfile
+
+For each affect dump:
+ Get the affect name
+ Make a subdir with that affect name
+ run the pipe in that subdir (separate method)
+ 
+Running a pipe:
+ make another subdir with the pipe's name, plus any non-global options given to the pipe
+  (i.e. running HeatSeg with a different time window)
+ 
+ run the process, output to the subdir
+ call gen_csv on the generated report file, prepend the returned string with the affect, pipe and options
+ 
+ 
+"""
+
+
+
