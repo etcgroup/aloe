95f092c9355570ef0781d550b7932a08545e8392
Sayer Rippey <srippey@oberlin.edu>
2013-05-29 13:35:34 -0700

    using the french stemmer

--- src/etc/aloe/wt2013/FrSimpleStringToWordVector.java
+++ src/etc/aloe/wt2013/FrSimpleStringToWordVector.java
@@ -66,7 +66,7 @@ public class FrSimpleStringToWordVector extends StringToWordVector {
         private final Pattern nonsensePattern;
 
         public NoNonsenseStemmer(boolean useSnowball) {
-            if (false) {
+            if (useSnowball) {
                 this.snowball = new SnowballStemmer("french");//this is where I need to put in the name of a different stemmer
             }
 
